7e55e9279a159e25d1dd76238daba1779976004a
Rakia-Chawali <rakiamohamed117@gmail.com>
2022-12-13 21:13:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Counter from "./components/profile/Counter.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  state = {
+    show: true,
+  };
+
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button
+          onClick={this.handleShowPerson}
+         
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Toggle
+        </button>
+
+        {this.state.show ? <Counter /> : null}
+      </>
+    );
+  }
 }
 
 export default App;
